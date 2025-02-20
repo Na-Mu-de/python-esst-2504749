db0efc644646f7edd17ea98ad0c1de277defd011
Na-Mu-de <naira.mueller@uni-weimar.de>
2025-02-20 11:04:45 +0100

    Update uebungs_datei.py

--- uebungs_datei.py
+++ uebungs_datei.py
@@ -1,3 +1,8 @@
 # Das ist ein Kommentar in Ihrer Uebungsdatei
 
 print('Hallo Welt!')
+
+erste_variable = 3
+zweite_variabel = 2
+ergebnis_multiplikation = erste_variable * zweite_variable
+print ("Mein Ergebnis", ergebnis-multiplikation)
